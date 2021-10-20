b218384808e0bc254c01fda9cc124718499b165a
mohan-konem-123 <66741361+mohan-konem-123@users.noreply.github.com>
2021-10-20 12:41:20 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,9 +2,9 @@ public class sum {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+      int num1 = 5, num2 = 15, s;
+      s = num1 + num2;
 
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of these numbers: "+s);
    }
 }
